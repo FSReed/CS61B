aada111571bf2b27cfe66bd9f2245c4d3ba03ca9
FSReed <1714747596@qq.com>
2023-11-16 23:30:33 +0800

    Lab2 completed.

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -16,5 +16,15 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+
+        IntList lst2 = IntList.of(14, 16, 12, 4);
+        boolean changed2 = IntListExercises.squarePrimes(lst2);
+        assertEquals("14 -> 16 -> 12 -> 4", lst2.toString());
+        assertFalse(changed2);
+
+        IntList lst3 = IntList.of(3, 7, 17, 13);
+        boolean changed3 = IntListExercises.squarePrimes(lst3);
+        assertEquals("9 -> 49 -> 289 -> 169", lst3.toString());
+        assertTrue(changed3);
     }
 }
