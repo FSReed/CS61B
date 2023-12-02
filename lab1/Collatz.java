ae65c87d43f2183d07745a94341102652bf8e3ea
FS_Reed <136261437+FSReed@users.noreply.github.com>
2023-12-02 17:44:08 +0800

    Update Collatz.java to create the merge error.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -12,7 +12,7 @@ public class Collatz {
         if (n == 1) {
             return 1;
         } else if (n % 2 == 1) {
-            return 3 * n + 1;
+            return 2 * n + 1;
         } else {
             return n / 2;
         }
