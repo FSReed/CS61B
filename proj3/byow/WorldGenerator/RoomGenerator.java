8b6bfb31fabb9b6fa4a3f9935d8eff9434b2ec4c
FSReed <1714747596@qq.com>
2024-11-30 21:11:04 +0800

    Support randomly generate non-overlapping rooms.
    Issue: Rooms generated are unnatural. And as the number of rooms
    increases, the efficiency of the algorithm drops quickly.

--- proj3/byow/WorldGenerator/RoomGenerator.java
+++ proj3/byow/WorldGenerator/RoomGenerator.java
@@ -1,12 +1,14 @@
 package byow.WorldGenerator;
 
 import byow.TileEngine.*;
+import java.util.HashSet;
 
 public class RoomGenerator {
 
     private final TETile[][] world;
-    private final int WIDTH;
-    private final int HEIGHT;
+    private final int WIDTH; // The maximum width of the map
+    private final int HEIGHT; // The maximum height of the map
+    private final HashSet<Room> rooms; // Stores all the rooms in the map
 
     /** The constructor of Room Generator
      * It will modify the original TETile array
@@ -18,6 +20,7 @@ public class RoomGenerator {
         this.world = world;
         this.WIDTH = w;
         this.HEIGHT = h;
+        this.rooms = new HashSet<>();
     }
 
     /**
@@ -27,35 +30,73 @@ public class RoomGenerator {
      * @param width: Should be positive
      * @param height: Should be positive
      */
-    public void addRectangularRoom(int X, int Y, int width, int height) {
+    public boolean addRectangularRoom(int X, int Y, int width, int height) {
         assert X >= 0 && Y >= 0: "The coordinate of the origin should be non-negative";
-        assert width > 0 && height > 0: "Room's width and height should be positive";
+        assert width > 2 && height > 2: "The width and height should be bigger than 2";
 
         // Validate the width and height
-        width = Math.min(width, WIDTH - X - 2);
-        height = Math.min(height, HEIGHT - Y - 2);
-        if (width <= 0 || height <= 0) {
-            // Just return, don't generate this room
-            return;
+        width = Math.min(width, WIDTH - X);
+        height = Math.min(height, HEIGHT - Y);
+        if (width <= 2 || height <= 2) {
+            // This room cannot be generated
+            return false;
+        }
+
+        // Check for overlapping
+        Room newRoom = new Room(X, Y, width, height);
+        if (overlapping(newRoom)) {
+            // New room overlaps with one or more existing rooms
+            return false;
         }
 
         // Build walls
         // Horizontal
-        for (int i = X; i <= X + width + 1; i++) {
+        for (int i = X; i < X + width; i++) {
             world[i][Y] = Tileset.WALL;
-            world[i][Y + height + 1] = Tileset.WALL;
+            world[i][Y + height - 1] = Tileset.WALL;
         }
         // Vertical
-        for (int j = Y + 1; j <= Y + height; j++) {
+        for (int j = Y + 1; j < Y + height; j++) {
             world[X][j] = Tileset.WALL;
-            world[X + width + 1][j] = Tileset.WALL;
+            world[X + width - 1][j] = Tileset.WALL;
         }
 
         // Make floors
-        for (int i = X + 1; i <= X + width; i++) {
-            for (int j = Y + 1; j <= Y + height; j++) {
+        for (int i = X + 1; i < X + width - 1; i++) {
+            for (int j = Y + 1; j < Y + height - 1; j++) {
                 world[i][j] = Tileset.FLOOR;
             }
+        }
+
+        // New room generated. Add it to the room set
+        rooms.add(newRoom);
+        return true;
+    }
+
+    private boolean overlapping(Room newRoom) {
+        // Check boundaries, with each existing room
+        for (Room existingRoom: rooms) {
+            if (!(newRoom.X + newRoom.width <= existingRoom.X
+                    || newRoom.Y + newRoom.height <= existingRoom.Y
+                    || newRoom.X >= existingRoom.X + existingRoom.width
+                    || newRoom.Y >= existingRoom.Y + existingRoom.height)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class Room {
+        int width;
+        int height;
+        int X;
+        int Y;
+
+        Room(int x, int y, int w, int h) {
+            this.X = x;
+            this.Y = y;
+            this.width = w;
+            this.height = h;
         }
     }
 }
--- proj3/byow/WorldGenerator/RoomTest.java
+++ proj3/byow/WorldGenerator/RoomTest.java
@@ -4,6 +4,8 @@ import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
+import java.util.Random;
+
 public class RoomTest {
 
     private TERenderer ter;
@@ -20,19 +22,9 @@ public class RoomTest {
         ter.initialize(width, height);
         room.addRectangularRoom(0, 0, 20, 20);
         room.addRectangularRoom(22, 22, 5, 5);
-        room.addRectangularRoom(30, 30, 1, 1);
+        room.addRectangularRoom(30, 30, 3, 3);
         room.addRectangularRoom(22, 6, 12, 8);
         room.addRectangularRoom(30, 0, 7, 3);
-        ter.renderFrame(world);
-    }
-
-    public void BigWorld() {
-        width = 96;
-        height = 54;
-        TETile[][] world = InitWorld(width, height);
-        room.addRectangularRoom(30, 30, 50, 50);
-        room.addRectangularRoom(0, 39, 1, 1);
-        room.addRectangularRoom(37, 0, 1, 5);
         ter.renderFrame(world);
     }
 
@@ -41,9 +33,60 @@ public class RoomTest {
         height = 40;
         TETile[][] world = InitWorld(width, height);
         room.addRectangularRoom(30, 30, 50, 50);
-        room.addRectangularRoom(0, 39, 1, 1);
-        room.addRectangularRoom(37, 0, 1, 5);
+        room.addRectangularRoom(0, 39, 3, 3);
+        room.addRectangularRoom(37, 0, 3, 5);
         ter.renderFrame(world);
+    }
+
+    public void BigWorld() {
+        width = 96;
+        height = 54;
+        TETile[][] world = InitWorld(width, height);
+        room.addRectangularRoom(30, 30, 50, 50);
+        room.addRectangularRoom(0, 39, 3, 3);
+        room.addRectangularRoom(37, 0, 3, 5);
+        ter.renderFrame(world);
+    }
+
+    /**
+     * Generate random rooms on the world map
+     * @param iter: Used to test the efficiency of the algorithm
+     * @param ROOMS: Max room number
+     * @param ATTEMPT: Max attempt to make in each iteration
+     * @param min_size: Minimum room width(height)
+     * @param max_size: Maximum room width(height)
+     */
+    public void RandomRooms(int iter, int ROOMS, int ATTEMPT, int min_size, int max_size) {
+        width = 90;
+        height = 45;
+        Random currentSeed = new Random();
+        int total_room = 0;
+        int total_attempt = 0;
+        while (iter > 0) {
+            TETile[][] world = InitWorld(width, height);
+            Random RANDOM = new Random(currentSeed.nextInt());
+            int rooms = 0;
+            int attempts = 0;
+            while (rooms < ROOMS && attempts < ATTEMPT) {
+                int X = RANDOM.nextInt(width);
+                int Y = RANDOM.nextInt(height);
+                int width = RANDOM.nextInt(max_size) + min_size;
+                int height = RANDOM.nextInt(max_size) + min_size;
+                if (room.addRectangularRoom(X, Y, width, height)) {
+                    rooms += 1;
+                }
+                attempts += 1;
+            }
+            System.out.printf("Use %d iterations to generate %d rooms\n", attempts, rooms);
+//            ter.renderFrame(world);
+            iter -= 1;
+            total_room += rooms;
+            total_attempt += attempts;
+        }
+        System.out.printf("Average efficiency: %.2f rooms/attempt\n", (double) total_room / total_attempt);
+    }
+
+    public void GenerateTunnel(int length) {
     }
 
     private TETile[][] InitWorld(int width, int height) {
@@ -63,6 +106,7 @@ public class RoomTest {
         RoomTest tester = new RoomTest();
 //        tester.BasicRooms();
 //        tester.RoomOutOfBoundsTest();
-        tester.BigWorld();
+//        tester.BigWorld();
+        tester.RandomRooms(100, 100, 30, 5, 10);
     }
 }
